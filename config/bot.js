2522c5450b64f08f97f085d0320ae26399568efd
Kamalmahadev <67117161+Kamalmahadev@users.noreply.github.com>
2021-05-17 13:36:08 +0530

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQyNzkyMDYwMDMyNTE2MTAz.YJ6dRQ.bX_VrKgE6tcnA5Q936IZ4qKzy58',
+        prefix: 'U',
+        activity: 'WITH K Λ M Λ Ł',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
